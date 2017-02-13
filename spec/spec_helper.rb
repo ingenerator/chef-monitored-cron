dc2bcda63c05ee06f0ee73fb5d53ccef65dc4179
Andrew Coulton <andrew@ingenerator.com>
2017-02-13 15:40:42 +0000

    Configure default chefspec platform to avoid warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,8 @@ require 'chefspec/berkshelf'
 RSpec.configure do |c|
   c.filter_run(focus: true)
   c.run_all_when_everything_filtered = true
+
+  # Default platform / version to mock Ohai data from
+  c.platform = 'ubuntu'
+  c.version = '14.04'
 end
