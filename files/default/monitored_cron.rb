9c37076794a2a48302b244f7b056fb2d0336fbe5
Andrew Coulton <andrew@ingenerator.com>
2017-02-15 16:04:35 +0000

    Document the monitored_cron wrapper and finalise syslog options
    Use the LOG_CRON facility, and make the global exception handler
    more robust.

--- files/default/monitored_cron.rb
+++ files/default/monitored_cron.rb
@@ -1,14 +1,48 @@
 #!/usr/bin/env ruby
+#
+# This program wraps a child executable in use as a cron job, and:
+#  * Diverts all output to syslog (stdout => debug, stderr => warning)
+#  * Optionally, requires a lock so that only one instance of the job can run
+#    at any one time
+#  * Logs a confirmation message to syslog when the task successfully completes,
+#    or logs an error if it fails (nonzero exit or any output to stderr)
+#  * Optionally, pings an HTTP/s URL when the task successfully completes - this
+#    can be templated to inclde the runtime in seconds eg as a GET parameter
+#
+# It takes a single command argument, which is a path to a JSON job file with
+# configuration for the task.
+#
+# The job conig looks something like
+#
+#   {
+#     "command": "/path/to/script with --some=arguments && anything 'that works in shell'",
+#     "notify":  {"url": "https://my.tracker/ac823-ado?time=:runtime:"},
+#     "locking": {"lockrun": "/path/to/lockrun", "lock_dir": "/var/run/", "retries": 2, "sleep": 5}
+#   }
+#
+#
+# Log messages use the LOG_CRON syslog facility and a program name calculated
+# the basename of the job config file provided
+#
+# Author::    Andrew Coulton (mailto:andrew@ingenerator.com)
+# Copyright:: Copyright (c) 2017 inGenerator Ltd
+# License::   Apache 2.0
 require 'syslog/logger'
 require 'json'
 require 'open3'
 require 'net/http'
 
+# This class is responsible for executing the process
 class MonitoredCronRunner
-  READ_WAIT_TIME = 0.01
+  # Maximum buffer to read from stdout / stderr in one go
   READ_SIZE = 4096
-  DEFAULT_READ_TIMEOUT = 600
 
+  # Maximum time to sleep waiting for new child process output
+  # The longer this is, the more chance that you will have to wait if a process
+  # finishes without printing any additional output
+  DEFAULT_READ_TIMEOUT = 5
+
+  # Thrown if the job configuration is missing or invalid
   class InvalidConfigurationError < RuntimeError
     def initialize(file, source_error)
       super(
@@ -17,12 +51,18 @@ class MonitoredCronRunner
     end
   end
 
+  # Executes the wrapper and runs the command
+  #
+  # * +job_file+ - path to the job JSON config file
   def self.run(job_file)
     new(job_file).instance_eval { run }
   end
 
   private
 
+  # Initialises the wrapper
+  #
+  # * +job_file+ - path to the job JSON config file
   def initialize(job_file)
     @job_file       = job_file
     @job_name       = File.basename(job_file, '.json')
@@ -78,6 +118,9 @@ class MonitoredCronRunner
     end
   end
 
+  # Builds the actual command to execute based on the job's `command` config
+  # and any locking configuration which will be used to prefix the main command
+  # with the arguments required to run it inside lockrun.
   def build_wrapped_command
     command = @config['command']
 
@@ -94,6 +137,7 @@ class MonitoredCronRunner
     command
   end
 
+  # The path to the lockfile lockrun should use
   def lockrun_lockfile
     dir = @config['locking']['lock_dir'].chomp('/')
     "#{dir}/cron-#{@job_name}.lock"
@@ -123,6 +167,7 @@ class MonitoredCronRunner
     exit_status
   end
 
+  # Captures all command output, sleeping whenever there is nothing new to read
   def log_command_output(stdout, stderr, timeout)
     output = read_pipe_lines(stdout, stderr, timeout)
 
@@ -136,6 +181,8 @@ class MonitoredCronRunner
     end
   end
 
+  # Reads the current content of stdout and stderr (if any) as an array of lines
+  # for each stream
   def read_pipe_lines(stdout, stderr, timeout)
     result = { stdout: [], stderr: [] }
     ready_pipes = IO.select([stdout, stderr], nil, nil, timeout)
@@ -156,17 +203,22 @@ class MonitoredCronRunner
       end
     rescue Errno::EAGAIN
     rescue EOFError
-      # Empty pipe, wait till next time
+      # Empty pipe, ignore this here so we can drop back to check if the
+      # process is still running and/or wait for further output
     end
 
     buffer.split("\n")
   end
 
+  # Actually logs the output to syslog
+  # Falls back to logging to console if there are any problems writing to the
+  # syslog, in the hope cron might be able to email it to someone useful
   def log(level, format_string, *format_args)
-    @syslog ||= Syslog.open(@syslog_program)
+    @syslog ||= Syslog.open(@syslog_program, Syslog::LOG_CONS, Syslog::LOG_CRON)
     @syslog.log(level, format_string, *format_args)
   end
 
+  # Handle the task exit status and report overall state
   def report_status(exit_status)
     if !exit_status.success?
       log(Syslog::LOG_ERR, 'Failed with exit code %d after %ss', exit_status.exitstatus, task_runtime_seconds)
@@ -190,14 +242,25 @@ class MonitoredCronRunner
     Net::HTTP.get(URI(url))
     log(Syslog::LOG_INFO, 'Pinged %s', url)
   end
-end
+end # /MonitoredCronRunner
 
+# This section is the script `main` executable. The conditional is to make it run
+# if you are executing this script directly, but not if the class is being included
+# eg from rspec for testing
 if $PROGRAM_NAME == __FILE__
   begin
     MonitoredCronRunner.run(ARGV[0])
   rescue Exception => err
-    syslog = Syslog.open('monitored-cron')
-    syslog.log(
+    # Rescuing Exception is usually considered bad practice, but we want to do
+    # as much as we can to try and get any and all issues reported via Syslog
+    # since it's likely anything printed to console / elsewhere won't make it
+    # to the end user.
+    # If we're not able to log to syslog here then it will spit out output and
+    # hopefully the cron daemon will be able to email it to someone that can fix
+    # it.
+    Syslog.close if Syslog.opened?
+    Syslog.open('monitored-cron', Syslog::LOG_CONS | Syslog::LOG_NDELAY, Syslog::LOG_CRON)
+    Syslog.log(
       Syslog::LOG_EMERG,
       'Unexpected exception [%s] %s in %s',
       err.class.name,
