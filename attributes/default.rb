d2a573500c99513653af7384fdc4d3e7572fe0c4
Andrew Coulton <andrew@ingenerator.com>
2017-02-15 23:24:30 +0000

    Fix typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['monitored_cron']['src_dir'] = '/usr/local/src/monintored_cron'
+default['monitored_cron']['src_dir'] = '/usr/local/src/monitored_cron'
 default['monitored_cron']['bin_dir'] = '/usr/local/bin'
