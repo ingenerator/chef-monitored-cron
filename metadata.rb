d820d52952e5d7ce56c5d9b46d68157dd9b07f68
Andrew Coulton <andrew@ingenerator.com>
2017-05-04 16:13:35 +0100

    Update metadata for 0.1.1 release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andrew@ingenerator.com'
 license 'Apache 2.0'
 description 'Wraps cron jobs to provide logging, remote monitoring and alerting'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 issues_url 'https://github.com/ingenerator/chef-monitored-cron/issues'
 source_url 'https://github.com/ingenerator/chef-monitored-cron'
 
